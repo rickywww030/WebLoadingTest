77d6c76d25bd4e1e6bd2ebc896bd559b6525cd34
rickywww030 <93711432+rickywww030@users.noreply.github.com>
2022-04-12 18:25:55 +0800

    T04

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -2,16 +2,13 @@ function UnityProgress(unityInstance, progress) {
   if (!unityInstance.Module)
     return;
 
-
-    /*Logo顯示*/
-    if (!unityInstance.logo)
+ if (!unityInstance.logo)
     {
         unityInstance.logo = document.getElementById("custom-logo");
         unityInstance.logo.style.display = "block";
         unityInstance.container.appendChild(unityInstance.logo);
     }
 
-    /*跑條顯示*/
     if (!unityInstance.progress) 
     {
     unityInstance.progress = document.getElementById("custom-loader");
@@ -23,41 +20,11 @@ function UnityProgress(unityInstance, progress) {
 
     if (progress == 1)
     {
-        unityInstance.logo.style.display = "none";/*載入完成後將Logo隱藏*/
-        unityInstance.progress.style.display = "none";/*載入完成後將跑條隱藏*/
+        unityInstance.logo.style.display = "none";
+        unityInstance.progress.style.display = "none";
     }
-
-
-    
-
-
-
-/*
-  if (!unityInstance.logo) {
-    unityInstance.logo = document.createElement("div");
-    unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
-    unityInstance.container.appendChild(unityInstance.logo);
-  }
-  if (!unityInstance.progress) {    
-    unityInstance.progress = document.createElement("div");
-    unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
-    unityInstance.progress.empty = document.createElement("div");
-    unityInstance.progress.empty.className = "empty";
-    unityInstance.progress.appendChild(unityInstance.progress.empty);
-    unityInstance.progress.full = document.createElement("div");
-    unityInstance.progress.full.className = "full";
-    unityInstance.progress.appendChild(unityInstance.progress.full);
-    unityInstance.container.appendChild(unityInstance.progress);
-  }
-  unityInstance.progress.full.style.width = (100 * progress) + "%";
-  unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
-    unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
-    */
 }
 
-
-    //value 參數為0-1
 function setLeaderProgressTo(value)
 {
     const fill = unityInstance.progress.getElementsByClassName("fill")[0];
@@ -76,4 +43,3 @@ function setLeaderProgressTo(value)
 
     fillText.textContent = (value*100).toFixed() +"%";    
 }
-
